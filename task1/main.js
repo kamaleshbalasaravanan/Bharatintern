1c96dd540c75a5749b106b7d2c60f3c662fa8aff
kamaleshwaran2002 <ks110302@gmail.com>
2023-09-20 19:04:23 +0530

    add checked and delete func

--- task1/main.js
+++ task1/main.js
@@ -4,13 +4,13 @@ const submitBtn = document.getElementById("submit-btn");
 
 const addTask = (e) => {
   e.preventDefault();
-  console.log("succesfully");
+  console.log("successfully");
 
   if (input.value === "") {
     alert("You must write something");
   } else {
     let li = document.createElement("li");
-    li.classList.add("list-group-item", "container-fluid", "col-11", "m-auto"); // Separate class names with commas
+    li.classList.add("list-group-item", "container-fluid", "col-11", "m-auto");
 
     let div = document.createElement("div");
     div.classList.add("input-group");
@@ -18,27 +18,14 @@ const addTask = (e) => {
     let inputfield = document.createElement("input");
     inputfield.setAttribute("type", "text");
     inputfield.setAttribute("value", `${input.value}`);
-    inputfield.classList.add("form-control", "no-border", "input-txt"); // Fix the class attribute
+    inputfield.classList.add("form-control", "no-border", "input-txt");
 
     inputfield.setAttribute("placeholder", "Username");
     inputfield.setAttribute("aria-label", "Username");
     inputfield.setAttribute("aria-describedby", "basic-addon1");
 
-    let span1 = document.createElement("span");
-    span1.classList.add("text");
-    span1.classList.add("no-border");
-    span1.classList.add("m-2");
-    let i1 = document.createElement("i");
-    i1.classList.add("fa-solid", "fa-check", "green-txt");
-    span1.appendChild(i1);
-
-    let span2 = document.createElement("span");
-    span2.classList.add("text");
-    span2.classList.add("no-border");
-    span2.classList.add("m-2");
-    let i2 = document.createElement("i");
-    i2.classList.add("fa-solid", "fa-xmark", "red-txt");
-    span2.appendChild(i2);
+    let span1 = createIconSpan("fa-check", "green-txt", "complete");
+    let span2 = createIconSpan("fa-xmark", "red-txt", "delete");
 
     div.appendChild(inputfield);
     div.appendChild(span1);
@@ -47,7 +34,57 @@ const addTask = (e) => {
     li.appendChild(div);
 
     listContainer.appendChild(li);
+
+    input.value = "";
+    saveData()
   }
 };
+
+function createIconSpan(iconClass, textClass, textContent) {
+  let span = document.createElement("span");
+  span.classList.add("text", "no-border", "m-2", textClass);
+
+  let i = document.createElement("i");
+  i.classList.add("fa-solid", iconClass, textContent);
+
+  span.appendChild(i);
+  return span;
+  
+}
+
+listContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    const taskItem = e.target.closest("li");
+    if (taskItem) {
+      listContainer.removeChild(taskItem);
+    }
+  }
+
+  saveData()
+});
+
+listContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("complete")) {
+      const inputField = e.target.closest("li").querySelector("input");
+      if (inputField) {
+        if (inputField.style.textDecoration === "line-through") {
+          inputField.style.textDecoration = "none";
+        } else {
+          inputField.style.textDecoration = "line-through";
+          inputField.disabled = true
+        }
+      }
+    }
+    saveData()
+  });
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+showTask()
 
 submitBtn.addEventListener("click", addTask);
